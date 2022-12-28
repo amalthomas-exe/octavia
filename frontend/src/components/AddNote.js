80c5836408ccd375cfb7fbf9d14ef35a8087a625
Amal Thomas <thomasamal479@gmail.com>
2022-12-28 20:47:04 +0530

    Updates nd minor fixes

--- frontend/src/components/AddNote.js
+++ frontend/src/components/AddNote.js
@@ -1,43 +1,43 @@
-import React ,{useState, useContext} from 'react'
+import React, { useState, useContext } from 'react'
 import noteContext from '../context/notes/noteContext';
 import './AddNote.css'
 
 const AddNote = () => {
   const context = useContext(noteContext);
-  const {addNote, setIsAddingNote} = context;
-  const [note,updateNote] = useState({"title":"","desc":""});
-  const handleChange = (e)=>{
-    updateNote(note=>({
+  const { theme, addNote, setIsAddingNote } = context;
+  const [note, updateNote] = useState({ "title": "", "desc": "" });
+  const handleChange = (e) => {
+    updateNote(note => ({
       ...note,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value
     }))
   }
 
-  const handleClick = (e)=>{
-    addNote(98,note.title,note.desc);
+  const handleClick = (e) => {
+    addNote(98, note.title, note.desc);
     setIsAddingNote(false);
   }
   return (
     <div id="page-body">
-      <div onClick={()=>{setIsAddingNote(false)}} id="btn-close"><i className="fa-solid fa-xmark"></i></div>
+      <div onClick={() => { setIsAddingNote(false) }} id="btn-close"><i className="fa-solid fa-xmark"></i></div>
       <div id="modal-box">
         <div id="preview-box">
-          {(note.title==="" && note.desc==="")?<div id="no-note-text" className='text-dark'>Add a note</div>:
-          <div>
-            <div id="preview-title" className=' text-dark'>{note.title}</div>
-            <div id="preview-desc" className=' text-dark'>{note.desc}</div>
-          </div>
+          {(note.title === "" && note.desc === "") ? <div id="no-note-text" className={(theme === "light") ? "" : 'text-dark'}>Add a note</div> :
+            <div>
+              <div id="preview-title" className={(theme === "light") ? "" : 'text-dark'}>{note.title}</div>
+              <div id="preview-desc" className={(theme === "light") ? "" : 'text-dark'}>{note.desc}</div>
+            </div>
           }
         </div>
-        <div id="add-note-box" className='dark-box-container'>
+        <div id="add-note-box" className={(theme === "light") ? "light-box-container" : 'dark-box-container'}>
           <div id="add-note-div">
             <div className="entry-field">
               <div className="text-small">Title</div>
-              <input onChange={handleChange} className="entry-field-input field-dark" type="text" name="title" id="title" />
+              <input onChange={handleChange} className={`entry-field-input ${(theme === "light") ? "" : "field-dark"}`} type="text" name="title" id="title" />
             </div>
             <div className="entry-field">
               <div className="text-small">Description</div>
-              <textarea onChange={handleChange} className="entry-field-input field-dark" type="text" name="desc" id="desc" />
+              <textarea onChange={handleChange} className={`entry-field-input ${(theme === "light") ? "" : "field-dark"}`} type="text" name="desc" id="desc" />
             </div>
             <div id="submit-btn-div">
               <button id="submit-btn" onClick={handleClick}>Add note</button>
--- frontend/src/components/NoteItem.js
+++ frontend/src/components/NoteItem.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import noteContext from '../context/notes/noteContext'
 import './NoteItem.css'
 
 const NoteItem = (props) => {
+  const context = useContext(noteContext);
+  const {setIsAddingNote} = context;
   return (
         <div id="note-item-box">
-            <div id="note-title">{props.title}</div>
+          <div id="note-header">
+          <div id="note-title">{props.title}</div>
+          <div id="note-controls">
+            <div className="control-btn" onClick={()=>{setIsAddingNote(true)}}><i class="fa-solid fa-pen-to-square"></i></div>
+            <div className="control-btn"><i class="fa-solid fa-trash"></i></div>
+          </div>
+          </div>
             <div id="note-desc">{props.desc}</div>
         </div>
   )
